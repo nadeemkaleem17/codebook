97a4b6c519117963010feda6b675e0b566c0c549
Nadeem <bscs20009@itu.edu.pk>
2024-11-13 15:44:27 +0500

    react toastify Update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+
 import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 import { ScrollToTop } from "./components";
 import { FilterProvider, CartProvider } from "./context";
